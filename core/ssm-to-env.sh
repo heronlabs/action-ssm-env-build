55d072a1837e40419152391a213e64f427bb4c99
Lucas Lacerda <lucaslacerdacl@gmail.com>
2024-03-18 11:40:26 -0300

    #deploy

--- core/ssm-to-env.sh
+++ core/ssm-to-env.sh
@@ -2,9 +2,6 @@
 
 wget https://github.com/Droplr/aws-env/raw/master/bin/aws-env-linux-amd64 -O aws-env
 
-echo $AWS_REGION
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-echo $AWS_ENV_PATH
+touch .env
 
 eval $(./aws-env --recursive --format=dotenv > .env)
