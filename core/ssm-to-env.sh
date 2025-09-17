c1e7d5d7be803952af0d725d37a7b2ff4cffb1a2
Lucas Lacerda <lucaslacerdacl@gmail.com>
2025-09-17 13:09:35 -0300

    #deploy

--- core/ssm-to-env.sh
+++ core/ssm-to-env.sh
@@ -3,7 +3,20 @@
 set -e
 
 wget https://github.com/Droplr/aws-env/raw/master/bin/aws-env-linux-amd64 -O aws-env
-
 chmod +x aws-env
 
-eval $(./aws-env --recursive --format=dotenv --role-arn="$AWS_ROLE_TO_ASSUME" > .env)
+SESSION=$(aws sts assume-role \
+--role-arn "$AWS_ASSUME_ROLE" \
+--role-session-name "$AWS_ASSUME_ROLE_SESSION" \
+--query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
+--output text)
+
+export AWS_ACCESS_KEY_ID=$(echo $SESSION | cut -d' ' -f1)
+export AWS_SECRET_ACCESS_KEY=$(echo $SESSION | cut -d' ' -f2)
+export AWS_SESSION_TOKEN=$(echo $SESSION | cut -d' ' -f3)
+
+eval $(./aws-env --recursive --format=dotenv > .env)
+
+export AWS_ACCESS_KEY_ID=''
+export AWS_SECRET_ACCESS_KEY=''
+export AWS_SESSION_TOKEN=''
