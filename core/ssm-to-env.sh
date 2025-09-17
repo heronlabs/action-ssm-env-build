b8fe5086883e9853adc0bdaeb654518b3f87d4b1
Lucas Lacerda <lucaslacerdacl@gmail.com>
2025-09-17 12:51:33 -0300

    #deploy

--- core/ssm-to-env.sh
+++ core/ssm-to-env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 wget https://github.com/Droplr/aws-env/raw/master/bin/aws-env-linux-amd64 -O aws-env
 
 chmod +x aws-env
 
-eval $(./aws-env --recursive --format=dotenv > .env)
+eval $(./aws-env --recursive --format=dotenv --role-arn="$AWS_ROLE_TO_ASSUME" > .env)
